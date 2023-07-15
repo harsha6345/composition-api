861d632ceb8370f44a13aa2bf87280b7d1cca447
harsha6345 <harshab6345@gmail.com>
2023-07-16 01:00:42 +0530

    Added all the files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script>
+import TheWelcome from '../components/TheWelcome.vue'
+import {onMounted} from "vue";
+
+export default {
+  components : { TheWelcome },
+
+  setup() {
+
+    onMounted(()=> {
+      alert("Hi there!")
+    })
+
+    return {
+      message : "Hello world",
+    };
+  },
+}
+
+
+</script>
+
+
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{ message }}</p>
+  </main>
+</template>
