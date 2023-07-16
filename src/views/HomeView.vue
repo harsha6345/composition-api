477de07c5ae0eb6ff33a20a1a9532ef52c0532e5
harsha6345 <harshab6345@gmail.com>
2023-07-16 23:13:28 +0530

    Local storage with composables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import {ref , watch} from "vue";
-
-
-let food = ref(localStorage.getItem('food'));
-let age = ref(localStorage.getItem('age'));
-
-watch(food , (val) => {
-  write('food', val);
-});
-
-watch(age , (val)=> {
-  write('age', val);
-})
-
-function write (key , val) {
-  localStorage.setItem(key , val);
-}
-
+  import { useStorage } from "@/composables/useStorage";
+  let food = useStorage('food', 'tacos');
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    <p>What is your favourite food? <input type="text" v-model="food"></p>
-    <p>How old are you? <input type="text" v-model="age"></p>
-
+    <p>
+      What is your favorite food? <input type="text" v-model="food">
+    </p>
   </main>
 </template>
