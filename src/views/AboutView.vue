c72d6138f22c2c798baeeec10aa57b0413254a2e
harsha6345 <harshab6345@gmail.com>
2023-07-16 09:58:54 +0530

    added flash message to the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,15 @@
+<script setup>
+import { useFlash } from "@/composables/useFlash"
+
+const { flash } = useFlash();
+
+</script>
+
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="flash('It works from the about page')">Click for the alert dude</button>
   </div>
 </template>
 
